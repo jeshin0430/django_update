710ee7d183bfcf5c794f05fc33a1006f853b9093
jeonghyun <tlsdhfcod@naver.com>
2020-05-25 13:14:48 +0900

    update

---
+++ todolist/blog/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+import datetime
+# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('deadline')
+    return render(request, 'home.html', {'posts' : posts})
+
+def new(request):
+    if request.method == 'POST':
+        new_post = Post.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            deadline = request.POST['deadline']
+        )
+        return redirect('detail', new_post.pk)
+    return render(request, 'new.html',)
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+
+    if request.method == "POST":
+        Comment.objects.create(
+            post = post,
+            content = request.POST['content']
+        )
+        return redirect('detail', post_pk)
+
+    return render(request, 'detail.html', {'post': post})
+
+def delete_comment(request, post_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('detail', post_pk)
+
+def edit(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.method == 'POST':
+        Post.objects.filter(pk=post_pk).update(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            deadline = request.POST['deadline']
+        )
+        return redirect('detail', post_pk)
+    return render(request, 'edit.html',)
+
+def delete(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+    return redirect('home')
